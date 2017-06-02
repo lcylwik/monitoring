bafe5adacb97616e3766e7176224c518296c7d75
Lianet <cylwiklianet@gmail.com>
2017-06-01 19:12:40 -0500

    vers

--- src/main/java/com/mkyong/web/controller/HelloController.java
+++ src/main/java/com/mkyong/web/controller/HelloController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-//api rest lianet
+//api rest 
 
 @RestController
 public class HelloController {
